93bab763f607280f1298dff942c221b6704a1451
TuqMt <forostanmatvej74@gmail.com>
2025-07-02 21:30:39 +0500

    Update check_subscriptions.py

--- pgl_bot/check_subscriptions.py
+++ pgl_bot/check_subscriptions.py
@@ -5,15 +5,14 @@ def check_subscriptions():
     conn = get_connection()
     cur = conn.cursor()
 
-
     now = int(time.time())
-
+    subscription_duration = 60
 
     cur.execute("SELECT login, date FROM tg WHERE isbuy = TRUE")
     users = cur.fetchall()
 
     for login, timestamp in users:
-        if now - timestamp > 86400:
+        if now - timestamp > subscription_duration:
             cur.execute("UPDATE tg SET isbuy = FALSE WHERE login = %s", (login,))
             print(f"Подписка у пользователя {login} завершена.")
 
